417f340360547d19768ad76fa68cb0ada1b10473
JanaVPetrova <janavpetrova@gmail.com>
2013-07-04 09:17:59 +0200

    try to add coveralls

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'rake'
 
 gem 'minitest'
 gem 'simplecov', :require => false, :group => :test
+gem 'coveralls', require: false
--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'minitest/autorun'
 require 'minitest/unit'
 require 'problems'
